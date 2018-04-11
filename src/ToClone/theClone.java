2656e2d08acc8fbf92edfbf6fc8ccb11636a0eba
Abbey <Abbey2009GH@thesnake.ca>
2018-04-11 12:56:38 -0300

    Added feeling fine message

--- src/ToClone/theClone.java
+++ src/ToClone/theClone.java
@@ -5,7 +5,7 @@ public class theClone {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("I am a clone!");
+System.out.println("I am a clone and I feel fine!");
 	}
 
 }
